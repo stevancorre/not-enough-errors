00ef22239bb793d6eb9dae7c0616683ae141693a
Stévan Corre <stevancorre@protonmail.com>
2022-06-03 05:01:41 +0200

    [KeyNotFoundError] rename parameter

--- src/errors/KeyNotFoundError.ts
+++ src/errors/KeyNotFoundError.ts
@@ -7,10 +7,10 @@ export class KeyNotFoundError extends BaseError {
     /**
      * Thrown when the key specified for accessing an element in a collection does not match any key in the collection
      * 
-     * @param keyName The key that causes this error
+     * @param key The key that causes this error
      * @param message The error message
      */
-    public constructor(keyName: string, message?: string) {
-        super("Key name", keyName, message ?? "The provided key cannot be found in the collection");
+    public constructor(key: string, message?: string) {
+        super("Key name", key, message ?? "The provided key cannot be found in the collection");
     }
 }
